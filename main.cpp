b052d9fdb7a60c277bb37027864afe32a3794ede
Sergey-Shl <Sergeishlyapnikov@gmail.com>
2015-04-19 20:43:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-	if (argc > 1)
+	if (argc != 1)
 	{
-		if (argv[1] == "-h")
+		//cout << argv[1] << endl;
+		if (argv[1] == "h")
 		{
 			cout << "It is help" << endl;
 			system("pause");
 			return 0;
 		}
 	}
-
 
 	db_store Shop(77, "Item 4", 0, 99, 2, 4);
 	cout << "Count: " << Shop.getCount() << endl;
